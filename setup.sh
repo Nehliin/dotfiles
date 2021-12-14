fdb1083124e9448a9500ddadff8c78761dd0efab
Nehliin <oskar.d.nehlin@gmail.com>
2021-12-14 21:29:19 +0100

    update setup script

--- setup.sh
+++ setup.sh
@@ -5,12 +5,12 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-if [ -z $2 ]; then
-    echo "work email not set"
-    exit 1
-fi
+#if [ -z $2 ]; then
+#    echo "work email not set"
+#    exit 1
+#fi
 
-if [ -z $3 ]; then
+if [ -z $2 ]; then
     echo "password not set"
     exit 1
 fi
@@ -22,41 +22,35 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 printf "Installing rust"
 curl https:/sh.rustup.rs -sSf | sh -s -- -y
+source "$HOME/.cargo/env"
 
 brew install neovim
 brew install lazygit 
+brew install alacritty
 
 cargo install ripgrep
 
-# setup git 
-mkdir ~/work
-mkdir ~/private
+sudo apt-get install tmux -y
 
+# setup git 
 mkdir -p ~/.ssh
 
 ssh-keygen -t ed25519 -C "$1" -f ~/.ssh/private -N "$3" 
-ssh-keygen -t ed25519 -C "$2" -f ~/.ssh/work -N "$3" 
 
 eval "$(ssh-agent -s)" && \
 ssh-add ~/.ssh/private
-ssh-add ~/.ssh/work
 
 touch ~/.ssh/config
 
 printf "Host *\nAddKeysToAgent yes\n " >> ~/.ssh/config
-  #IdentityFile ~/.ssh/work"
 
 touch ~/.gitconfig
 
-printf "[includeIf \"gitdir:~/private/\"]\npath = ~/private/.gitconfig-priv\n[includeIf \"gitdir:~/work/\"]\npath = ~/work/.gitconfig-work\n\n[core]\nexcludesfile = ~/.gitignore" >> ~/.gitconfig
+#printf "[includeIf \"gitdir:~/private/\"]\npath = ~/private/.gitconfig-priv\n[includeIf \"gitdir:~/work/\"]\npath = ~/work/.gitconfig-work\n\n[core]\nexcludesfile = ~/.gitignore" >> ~/.gitconfig
 
-touch ~/private/.gitconfig.priv
+#touch ~/private/.gitconfig.priv
 
-printf "[user]\nemail = $1\n name = Nehliin\n\n[github]\nuser = \"Nehliin\"\n\n[core]\nsshCommand = \"ssh -i ~/.ssh/private\"" >> ~/private/.gitconfig.priv
-
-touch ~/work/.gitconfig.work
-
-printf "[user]\nemail = $2\n name = oskarn\n\n[github]\nuser =\"oskarn\"\n\n[core]\nsshCommand = \"ssh -i ~/.ssh/work\"" >> ~/work/.gitconfig.work
+printf "[user]\nemail = $1\n name = Nehliin\n\n[github]\nuser = \"Nehliin\"\n\n[core]\nsshCommand = \"ssh -i ~/.ssh/private\"" >> ~/.gitconfig
 
 sudo apt-get install zsh -y 
 # Set shell to zsh 
@@ -70,6 +64,17 @@ git clone https://github.com/Nehliin/dotfiles.git
 
 rm ~/.zshrc
 ln -s "$(pwd)/dotfiles/.zshrc" ~/.zshrc
+ln -s "$(pwd)/dotfiles/tmux.conf" ~/.tmux.conf
+
 mkdir -p ~/.config
 ln -s "$(pwd)/dotfiles/nvim" ~/.config/nvim  
+ln -s "$(pwd)/dotfiles/alacritty" ~/.config/alacritty
+
+# set up rust-analyzer
+cd ~/Desktop
+git clone https://github.com/rust-analyzer/rust-analyzer.git
+cd rust-analyzer && cargo xtask install --server
+
+printf "Set up complete! Here is the git key"
+cat ~/.ssh/private.pub
 
